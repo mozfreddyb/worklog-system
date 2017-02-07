ac6490defa867a207568a8f0ab90fbaa2fb39814
Frederik Braun <fbraun+gh@mozilla.com>
2017-02-07 11:08:46 +0100

    import highlight.js only once in worker

--- js/worker.js
+++ js/worker.js
@@ -1,5 +1,5 @@
+importScripts('highlight.pack.js');
 onmessage = function(event) {
-  importScripts('highlight.pack.js');
   var request = event.data;
   var result = self.hljs.highlightAuto(request.code);
   postMessage({ hl: result.value, id: request.id});
